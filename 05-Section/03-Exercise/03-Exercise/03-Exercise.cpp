c535b8924ab29bc2d47ec0c9c8a7ca3a331dd09d
massije <jedc.marti@gmail.com>
2022-11-13 16:13:17 +0100

    05-Section --> 03-Exercise-Finished

---
+++ 05-Section/03-Exercise/03-Exercise/03-Exercise.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int a = 0, b = 0, c = 0;
+    float r1 = 0, r2 = 0;
+
+    cout << "Enter a: ";
+    cin >> a;
+
+    cout << "Enter b: ";
+    cin >> b;
+
+    cout << "Enter c: ";
+    cin >> c;
+
+    r1 = (float)(-b + sqrt(pow(b, 2) - 4 * a * c)) / 2 * a;
+    r2 = (float)(-b - sqrt(pow(b, 2) - 4 * a * c)) / 2 * a;
+
+    cout << "Roats are:" << endl << "r1 = " << r1 << endl << "r2 = " << r2 << endl;
+
+    return 0;
+    
+}
+
+
