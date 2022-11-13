f966c13a0f0ba82d378dca0cf1fa9def17e172e6
massije <jedc.marti@gmail.com>
2022-11-13 15:09:41 +0100

    05-Section --> 01-Exercise-Finished

---
+++ 05-Section/01-Exercise/01-Exercise/01-Exercise.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int b = 7, h = 5;
+    float area;
+
+    area = (float)(b * h) / 2;
+
+    cout << "The area is: " << area << endl;
+}
+
