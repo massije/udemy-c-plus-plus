af3f8f98f9544b522c2b21f363ad229a0c1312f5
massije <jedc.marti@gmail.com>
2023-01-01 21:00:19 +0100

    06-Section --> 12-Exercise-Finished

---
+++ 06-Section/12-Exercise/12-Exercise/12-Exercise/12-Exercise.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int d = 0;
+
+    cout << "Enter a number: ";
+    cin >> d;
+    cout << endl;
+
+    if (d == 1) {
+        cout << "one" << endl;
+    }
+    else if (d == 2) {
+        cout << "two" << endl;
+    }
+    else if (d == 3) {
+        cout << "three" << endl;
+    }
+    else if (d == 4) {
+        cout << "four" << endl;
+    }
+    else if (d == 5) {
+        cout << "five" << endl;
+    }
+    else if (d == 6) {
+        cout << "six" << endl;
+    }
+    else if (d == 7) {
+        cout << "seven" << endl;
+    }
+    else if (d == 8) {
+        cout << "eight" << endl;
+    }
+    else if (d == 9) {
+        cout << "nine" << endl;
+    }
+    else{
+        cout << "zero" << endl;
+    }
+
+    return 0;
+}
+
+
